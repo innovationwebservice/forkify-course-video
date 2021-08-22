e4449ae55a1d4ccfa0cada69d7b91478c91b5d11
innovationwebservice <innovation.web.services@gmail.com>
2021-08-21 17:36:45 -0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f4b19946-f7c5-4633-992f-8c7117637dbc';
+export const MODAL_CLOSE_SEC = 2.5;
